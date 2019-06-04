5ef6485be9949a2995b1b9d4b4919f88d698936b
James Myatt <james@jamesmyatt.co.uk>
2019-06-04 11:01:38 +0100

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
 dir /w %LIBRARY_INC%\graphviz
 dir /w %LIBRARY_LIB%\graphviz
 echo START INSTALL
-python setup.py install --single-version-externally-managed --record=record.txt --include-path=%LIBRARY_INC%\graphviz --library-path=%LIBRARY_LIB%/graphviz
+python setup.py install --single-version-externally-managed --record=record.txt --include-path=%LIBRARY_INC%\graphviz --library-path=%LIBRARY_LIB%\graphviz
